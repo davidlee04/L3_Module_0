f3baea0cbd8eb92b21bdcc8fed70c9e63c0f7072
pigchick3n <league@WTS21.attlocal.net>
2017-10-20 18:30:45 -0700

    intro

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -1,6 +1,15 @@
 package IntroToArrayLists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +18,53 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	JFrame frame;
+	JPanel panel;
+	JButton addName;
+	JButton viewName;
+	
+	ArrayList<String> input = new ArrayList<String>();
+	
+	public GuestBook () {
+		frame = new JFrame();
+		panel = new JPanel();
+		addName = new JButton();
+		viewName = new JButton();
+		
+		setUI();
+	}
+	public static void main(String[] args) {
+		GuestBook hi = new GuestBook();
+	}
+	
+	void setUI() {
+		frame.add(panel);
+		panel.add(addName);
+		panel.add(viewName);
+		frame.setVisible(true);
+		addName.addActionListener(this);
+		viewName.addActionListener(this);
+		addName.setText("Add A Name");
+		viewName.setText("View Names");
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == addName) {
+			String name = JOptionPane.showInputDialog("Enter a name.");
+			input.add(name);
+		}
+		if(e.getSource() == viewName) {
+			String s = "";
+			for(int i = 0; i<input.size(); i++) {
+				s += "Guest #"+(i+1)+": "+input.get(i)+"\n";
+				
+			}
+			    JOptionPane.showMessageDialog(null, s+"");
+		}
+	}
+
 }
