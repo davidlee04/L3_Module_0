c07801af6d1aeb254c84f98655231e6e6a43d765
davidlee04 <league@WTS22.attlocal.net>
2017-11-03 18:30:35 -0700

    stacks

--- src/IntroToStacks/IntroToStack.java
+++ src/IntroToStacks/IntroToStack.java
@@ -1,29 +1,52 @@
 package IntroToStacks;
 
+import java.util.Random;
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
 public class IntroToStack {
 	public static void main(String[] args) {
-		//1. Create a Stack of Doubles
-		//   Don't forget to import the Stack class
-		
-		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
-		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
-		//4. Pop all the elements off of the Stack. Every time a double is popped that is
-		//   between the two numbers entered by the user, print it to the screen.
-		
-		//   EXAMPLE:
-		//   NUM 1: 65
-		//   NUM 2: 75
-		
-		//   Popping elements off stack...
-		//   Elements between 65 and 75:
-		//   66.66876846
-		//   74.51651681
-		//   70.05110654
-		//   69.21350456
-		//   71.54506465
-		//   66.47984807
-		//   74.12121224
+		// 1. Create a Stack of Doubles
+		// Don't forget to import the Stack class
+		Stack<Double> stack = new Stack<Double>();
+
+		// 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+		for (int i = 0; i < 100; i++) {
+			Random random = new Random();
+			double d = random.nextDouble() * 100;
+			stack.push(d);
+		}
+
+		// 3. Ask the user to enter in two numbers between 0 and 100, inclusive.
+		String input1 = JOptionPane.showInputDialog("Enter a number between 0 and 100.");
+		String input2 = JOptionPane.showInputDialog("Enter a different number between 0 and 100.");
+		double one = Integer.parseInt(input1);
+		double two = Integer.parseInt(input2);
+
+		// 4. Pop all the elements off of the Stack. Every time a double is popped that
+		// is
+		// between the two numbers entered by the user, print it to the screen.
+		int size = stack.size();
+		for (int i = 0; i < size; i++) {
+			double pop = stack.pop();
+			if (one < pop && pop < two) {
+				System.out.println(pop + "");
+			}
+		}
+
+		// EXAMPLE:
+		// NUM 1: 65
+		// NUM 2: 75
+
+		// Popping elements off stack...
+		// Elements between 65 and 75:
+		// 66.66876846
+		// 74.51651681
+		// 70.05110654
+		// 69.21350456
+		// 71.54506465
+		// 66.47984807
+		// 74.12121224
 	}
 }
